5d5077e49ecc667872962b37d8bbd85fdabb8d7c
Alex Zhuravlev <leftemail17@mail.ru>
2024-07-28 13:55:31 +0300

    feat: add getting token from args

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	var token string
+	flag.StringVar(&token, "token", "", "token for access to telegram bot")
+
+	flag.Parse()
+
+	if token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return token
 }
